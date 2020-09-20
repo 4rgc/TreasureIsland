afce51e88c52a999e8c9c754555245318130af87
Andrew <justkrut514@gmail.com>
2020-09-20 09:00:07 -0400

    Wire the sign out button to API

---
+++ public/scripts/index.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+    console.log('loaded')
+    document.getElementById('signOut').onclick = () => {
+        console.log('siging out')
+        httpGetAsync('/login/deauthorize', (res) => {
+            let obj = JSON.parse(res);
+            if(!obj['success'])
+                throw new Error('Could not log out')
+            window.location = '/signin.html'
+        })
+    }
+}
