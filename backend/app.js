fc32b8247acba2a2bdf9cebde3bc766597e1220d
Andrew <justkrut514@gmail.com>
2020-09-24 14:27:10 -0400

    Fix URL and add CORS handling

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api')
 var loginRouter = require('./routes/login')
+var cors = require('cors')
 
 var app = express();
 
@@ -15,11 +16,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+var corsOptions = {
+  origin: 'http://api.treasureisland.tech',
+  optionsSuccessStatus: 200
+}
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
